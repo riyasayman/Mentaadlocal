78baf70fb87bf5f226f511c97f0382e35e27868b
riyasayman <30586982+riyasayman@users.noreply.github.com>
2017-10-10 10:44:21 +0530

    About page
    Worked about page and register form first page

---
+++ wordpress/wp-content/themes/mentaad/register.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Template Name: Register(no sidebar)
+ *
+ * This is the template that displays full width page without sidebar
+ *
+ * @package sparkling
+ */
+
+get_header(); ?>
+<div class="containerout">
+		<a href="#">التسجيل</a>
+		</div>
+  <div id="primary" class="content-area">
+
+	<main id="main" class="site-main" role="main">
+
+         <div class="container-inner">
+		 
+		 <div class="topbg">
+		 <h3>أسعار التذاكر</h3>
+		 <div class="price">
+ <div class="btn btn-default btn-lg btn-huge">195 ريال
+</div>
+		 </div>
+		 <div class="reg-contnet">
+		 <h4><span>التسجيل المبكر/ </span>سجل قبل تاريخ 22 ديسمبر بسعر 180 ريال.</h4>
+		 <hr/>
+		 <h4><span>تسجيل الجماعي/ </span>استفد من عرض التسجيل الجماعي لثلاثة مشاركين فأكثر واحصل على تخفيض 10.%</h4>
+		 <hr/>
+		 <h4><span>تسجيل الطلاب*/ </span>إذا كنت لازلت على مقاعد الدراسة تواصل معنا عبر البريد  reg@mentaad.com للحصول على
+<br/>رمز التخفيض الخاص بالطلاب.</h4>
+<h6 class="reg-req">* يتوفر 40 مقعد فقط بسعر تسجيل الطلاب</h6>
+		 </div>
+		 </div>
+		  <div class="bot-bg">
+		 <h3>أسعار التذاكر</h3>
+		<div class="row com-row reg-field-first">
+								 <div class="col-md-4 coup">
+ <input type="text" name="activate" placeholder="أدخل رمز التخفيض"><button class="coup_activate">تفعيل</button> 
+</div>
+</div>
+<div class="row com-row reg-field-first-spac">
+						 <div class="col-md-4">
+ <input type="text" name="firstname" placeholder="الأسم الأول">
+</div>
+						 <div class="col-md-4">
+<input type="text" name="lastname" placeholder="الأسم الأخير">
+</div>
+						 <div class="col-md-4 visible-xs">
+
+</div>
+</div>
+<div class="row com-row reg-field-first-spac">
+						 <div class="col-md-4">
+ <select>
+							  <option value="volvo">الجنس</option>
+							  <option value="saab">الإعلانات</option>
+							  <option value="mercedes">الرجوع من صديق</option>						
+								</select>
+</div>
+						 <div class="col-md-4">
+<select>
+							  <option value="volvo">المجال الفني</option>
+							  <option value="saab">الإعلانات</option>
+							  <option value="mercedes">الرجوع من صديق</option>						
+								</select>
+</div>
+
+						 <div class="col-md-4 visible-xs">
+
+</div>
+
+</div>
+<div class="row com-row reg-field-first-spac">
+						 <div class="col-md-4">
+ <select>
+							  <option value="volvo">المؤهل العلمي</option>
+							  <option value="saab">الإعلانات</option>
+							  <option value="mercedes">الرجوع من صديق</option>						
+								</select>
+</div>
+						 <div class="col-md-4">
+<select>
+							  <option value="volvo">الحالة</option>
+							  <option value="saab">الإعلانات</option>
+							  <option value="mercedes">الرجوع من صديق</option>						
+								</select>
+</div>
+
+						 <div class="col-md-4 visible-xs">
+
+</div>
+
+</div>
+<div class="row com-row reg-field-first-spac">
+						 <div class="col-md-4">
+ <input type="text" name="firstname" placeholder="الشركة / المؤسسة">
+</div>
+							 <div class="col-md-4">
+<select>
+							  <option value="volvo"> الدولة</option>
+							  <option value="saab">الإعلانات</option>
+							  <option value="mercedes">الرجوع من صديق</option>						
+								</select>
+</div>			 <div class="col-md-4 visible-xs">
+
+</div>
+</div>
+
+<div class="row com-row reg-field-first-spac">
+						 <div class="col-md-4">
+ <input type="text" name="firstname" placeholder="رقم الهاتف">
+</div>
+						 <div class="col-md-4">
+<input type="text" name="lastname" placeholder="البريد الإلكتروني">
+</div>
+	
+</div>
+ <h3>أسئلة تحليلية</h3>
+ <div class="row com-row reg-field-first-spac">
+
+							 <div class="col-md-4">
+<select>
+							  <option value="volvo"> كيف تعرفت على منطاد؟</option>
+							  <option value="saab">الإعلانات</option>
+							  <option value="mercedes">الرجوع من صديق</option>						
+								</select>
+</div>			
+</div>
+<h3>ما الذي ترغب في تحقيقه من خلال المشاركة في الحدث؟</h3>
+<div class="row">
+<a href="#" class="hfa-btn hfa-rtl">التالي</a>
+</div>
+							</div>
+		 </div> 
+		 </div>
+		 </div>
+	</main><!-- #main -->
+
+  </div><!-- #primary -->
+
+</div><!-- close .main-content-inner -->
+
+<?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/mentaad/speakers.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Template Name: About(no sidebar)
+ *
+ * This is the template that displays full width page without sidebar
+ *
+ * @package sparkling
+ */
+
+get_header(); ?>
+<div class="containerout">
+		<a href="#">المتحدثين</a>
+		</div>
+  <div id="primary" class="content-area">
+
+	<main id="main" class="site-main" role="main">
+
+         <div class="container-inner">
+		 
+		 <div class="top-content">
+		 <div class="row reg-contnet">
+
+     <div class="row row-margin-bottom">
+            <div class="col-md-12 lib-item">
+                <div class="lib-panel">
+                    <div class="row box-shadow">
+                        <div class="col-md-4">
+                            <img class="lib-img-show" src="http://localhost/wordpress/wp-content/uploads/2017/10/logo-1.png">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="lib-row lib-header">
+                                Example library
+                                <div class="lib-header-seperator"></div>
+                            </div>
+                            <div class="lib-row lib-desc">
+                                Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
+                            </div>
+                        </div>
+						
+                    </div>
+                </d
+	</div>
+		 </div>
+		 </div>
+		
+		 </div> 
+		 </div>
+		 </div>
+	</main><!-- #main -->
+
+  </div><!-- #primary -->
+
+</div><!-- close .main-content-inner -->
+
+<?php get_footer(); ?>
